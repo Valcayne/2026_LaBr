934bce40f16c67ba06984ea64c062c914dd0f9a0
Valcayne <valcayne@gmail.com>
2026-05-19 18:53:13 +0200

    f

--- Make2DHistos/Make2DHistos.cc
+++ Make2DHistos/Make2DHistos.cc
@@ -48,57 +48,6 @@ int main(int argc, char** argv) {
 
   //==================================================================================
 
-  HistoInfo* HistoInfoC6D6 = new HistoInfo();
-
-  HistoInfoC6D6->DetectorName = "C6D6";
-  HistoInfoC6D6->DetectorNumber = {0, 1, 2, 3, 4};
-  HistoInfoC6D6->DetectorDeadTime_ns = {0, 0, 0, 0, 0};
-
-  HistoInfoC6D6->HistoName.push_back("Edep");
-  HistoInfoC6D6->HistoTitle.push_back("Edep");
-  HistoInfoC6D6->HistoType.push_back(1);
-  HistoInfoC6D6->Xaxis.push_back({1e3, 1.e-3, 1.e7});
-  // HistoInfoC6D6->Yaxis.push_back({2e4, 0, 20});
-  HistoInfoC6D6->Yaxis.push_back({2e3, 0, 20});
-
-  HistoInfoC6D6->HistoName.push_back("En");
-  HistoInfoC6D6->HistoTitle.push_back("En");
-  HistoInfoC6D6->HistoType.push_back(1);
-
-  HistoInfoC6D6->Xaxis.push_back({1.8e5, 1.e-2, 1.e7});
-  HistoInfoC6D6->Yaxis.push_back({0.12, 0.15, 0.3, 0.5, 1, 10, 20});
-  if (IfGainCorrected) {
-    HistoInfoC6D6->HistoName.push_back("Edep_WF");
-    HistoInfoC6D6->HistoTitle.push_back("Edep_WF");
-    HistoInfoC6D6->HistoType.push_back(5);
-    HistoInfoC6D6->Xaxis.push_back({1e3, 1.e-3, 1.e7});
-    // HistoInfoC6D6->Yaxis.push_back({2e4, 0, 20});
-    HistoInfoC6D6->Yaxis.push_back({2e3, 0, 20});
-
-    HistoInfoC6D6->HistoName.push_back("En_WF");
-    HistoInfoC6D6->HistoTitle.push_back("En_WF");
-    HistoInfoC6D6->HistoType.push_back(5);
-
-    HistoInfoC6D6->Xaxis.push_back({1.8e5, 1.e-2, 1.e7});
-    HistoInfoC6D6->Yaxis.push_back({0.12, 0.15, 10, 20});
-  }
-  HistoInfoC6D6->HistoName.push_back("TFlash");
-  HistoInfoC6D6->HistoTitle.push_back("TFlash");
-  HistoInfoC6D6->HistoType.push_back(3);
-  HistoInfoC6D6->Xaxis.push_back({1e6, 0, 1.e6});
-  HistoInfoC6D6->Yaxis.push_back({1, 0, 2});
-  HistoInfoC6D6->HistoName.push_back("DeltaTOF");
-  HistoInfoC6D6->HistoTitle.push_back("DeltaTOF");
-  HistoInfoC6D6->HistoType.push_back(4);
-  HistoInfoC6D6->Xaxis.push_back({1e4, 0, 1.e4});
-  HistoInfoC6D6->Yaxis.push_back({400, 0, 20});
-
-  HistoInfoC6D6->HistoName.push_back("DetectorDeadTime_ns");
-  HistoInfoC6D6->HistoTitle.push_back("DetectorDeadTime_ns");
-  HistoInfoC6D6->HistoType.push_back(6);
-  HistoInfoC6D6->Xaxis.push_back({1, 0, 2});
-  HistoInfoC6D6->Yaxis.push_back({1, 0, 2});
-
   HistoInfo* HistoInfoSILI = new HistoInfo();
 
   HistoInfoSILI->DetectorName = "SILI";
@@ -135,29 +84,14 @@ int main(int argc, char** argv) {
   // HistoInfoSILI->Xaxis.push_back({10, 1.e-3, 1.e7});
   // HistoInfoSILI->Yaxis.push_back({200, 0, 1e5});
 
-  HistoInfo* HistoInfoFIMG = new HistoInfo();
-
-  // HistoInfoFIMG->DetectorName = "FIMG";
-  // HistoInfoFIMG->DetectorNumber = {0, 1, 2};
-  // HistoInfoFIMG->DetectorDeadTime_ns = {0, 0, 0};
-
-  // HistoInfoFIMG->HistoName.push_back("Edep");
-  // HistoInfoFIMG->HistoTitle.push_back("Edep");
-  // HistoInfoFIMG->HistoType.push_back(1);
-  // HistoInfoFIMG->Xaxis.push_back({100, 1.e-3, 1.e7});
-  // HistoInfoFIMG->Yaxis.push_back({1000, 0, 1e4});
-
-  // HistoInfoFIMG->HistoName.push_back("En");
-  // HistoInfoFIMG->HistoTitle.push_back("En");
-  // HistoInfoFIMG->HistoType.push_back(1);
-  // HistoInfoFIMG->Xaxis.push_back({1e4, 1.e-3, 1.e7});
-  // HistoInfoFIMG->Yaxis.push_back({2e1, 0, 1e4});
-
   HistoInfo* HistoInfoLABR = new HistoInfo();
 
   HistoInfoLABR->DetectorName = "LABR";
-  HistoInfoLABR->DetectorNumber = {0, 1, 2};
-  HistoInfoLABR->DetectorDeadTime_ns = {0, 0, 0};
+  HistoInfoLABR->DetectorNumber = {0, 1, 2, 3,  4,  5,  6,
+                                   7, 8, 9, 10, 11, 12, 13};
+  HistoInfoLABR->DetectorDeadTime_ns = {
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  };
 
   HistoInfoLABR->HistoName.push_back("Edep");
   HistoInfoLABR->HistoTitle.push_back("Edep");
@@ -222,25 +156,6 @@ int main(int argc, char** argv) {
     theCalibM->CalibrationManagerWF(WFFolder, RunListFname, RunNumber, 0);
   }
 
-  // Create tree and signal for each detector
-  TTree* trC6D6 = (TTree*)f1->Get(HistoInfoC6D6->DetectorName.c_str());
-  bool ThereIsC6D6Detector = false;
-  int nbunchesC6D6 = 0;
-  Long64_t bunchesIndexC6D6[MAXNBUNCHESINFILE];
-  int BunchNumberC6D6[MAXNBUNCHESINFILE];
-  Signal theC6D6S;
-  int NC6D6Signals = 0;
-  Signal* theC6D6Signals;
-  theC6D6Signals = new Signal[MAXNSIGNALSINPULSE];
-  bool UseAreaInsteadOfAmplitudeC6D6 = false;
-  if (!(trC6D6 == 0)) {
-    cout << "There is C6D6" << endl;
-
-    ThereIsC6D6Detector = true;
-    nbunchesC6D6 =
-        CreateBunchesIndex(trC6D6, bunchesIndexC6D6, BunchNumberC6D6);
-    AttachStruct(&theC6D6S, trC6D6, UseAreaInsteadOfAmplitudeC6D6);
-  }
   //==============================================================================
   //===============
   //===============================================================
@@ -265,25 +180,7 @@ int main(int argc, char** argv) {
   }
   //==============================================================================
   //==============================================================================
-  bool ThereIsFIMGDetector = false;
 
-  TTree* trFIMG = (TTree*)f1->Get(HistoInfoFIMG->DetectorName.c_str());
-  Signal theFIMGS;
-  int nbunchesFIMG = 0;
-  Long64_t bunchesIndexFIMG[MAXNBUNCHESINFILE];
-  int BunchNumberFIMG[MAXNBUNCHESINFILE];
-  int NFIMGSignals = 0;
-  Signal* theFIMGSignals;
-  bool UseAreaInsteadOfAmplitudeFIMG = false;
-
-  if (!trFIMG == 0) {
-    cout << "There is FIMG" << endl;
-    ThereIsFIMGDetector = true;
-    nbunchesFIMG =
-        CreateBunchesIndex(trFIMG, bunchesIndexFIMG, BunchNumberFIMG);
-    AttachStruct(&theFIMGS, trFIMG, UseAreaInsteadOfAmplitudeFIMG);
-    theFIMGSignals = new Signal[MAXNSIGNALSINPULSE];
-  }
   // //==============================================================================
   //==============================================================================
   bool ThereIsLABRDetector = true;
@@ -314,21 +211,10 @@ int main(int argc, char** argv) {
   TH1D* hPulIntens[N_PULSETYPE * 2];
   CreateTH1DHistoPulseIntesity(hPulIntens, N_PULSETYPE, fout);
 
-  // C6D6 histos
-
-  TH2D*** h2D_C6D6[N_PULSETYPE];
-  CreateTH2DHisto(ThereIsC6D6Detector, HistoInfoC6D6, N_PULSETYPE, h2D_C6D6,
-                  fout);
-
   // SILI histos
 
   TH2D*** h2D_SILI[N_PULSETYPE];
   CreateTH2DHisto(ThereIsSILIDetector, HistoInfoSILI, N_PULSETYPE, h2D_SILI,
-                  fout);
-  // FIMG histos
-
-  TH2D*** h2D_FIMG[N_PULSETYPE];
-  CreateTH2DHisto(ThereIsFIMGDetector, HistoInfoFIMG, N_PULSETYPE, h2D_FIMG,
                   fout);
 
   // LABR histos
@@ -354,18 +240,14 @@ int main(int argc, char** argv) {
     if (thePKUPInfo->pulseType[npul] == PARASITIC_PULSES_ID) {
       thisPulseType = 2;
     }
-    double C6D6TFlash = -1;
     double SILITFlash = -1;
-    double FIMGTFlash = -1;
     double LABRTFlash = -1;
 
     // cout << "Inte " << thePKUPInfo->PulseIntensity[npul] << endl;
     if (thePKUPInfo->PulseIntensity[npul] < 1.e12) {
       // There is not beam in this run so I force the Tflash to Zero for
       // security
-      C6D6TFlash = 0;
       SILITFlash = 0;
-      FIMGTFlash = 0;
       LABRTFlash = 0;
     }
 
@@ -373,29 +255,6 @@ int main(int argc, char** argv) {
         N_PULSETYPE, thisPulseType, thePKUPInfo->PulseIntensity[npul],
         thePKUPInfo->PKUPAmp[npul], thePKUPInfo->PKUPArea[npul], hPulIntens);
 
-    if (ThereIsC6D6Detector) {
-      NC6D6Signals =
-          GetSignals(HistoInfoC6D6->DetectorName,
-                     HistoInfoC6D6->DetectorDeadTime_ns, trC6D6, &theC6D6S,
-                     bunchesIndexC6D6[npul], bunchesIndexC6D6[npul + 1] - 1,
-                     theC6D6Signals, theCalibM, 1, C6D6TFlash);  //
-
-      FillType1(thisPulseType, NC6D6Signals, theC6D6Signals, h2D_C6D6, -1.e20,
-                1.e20, HistoInfoC6D6);
-
-      FillType2(thisPulseType, NC6D6Signals, theC6D6Signals, h2D_C6D6, -1.e20,
-                1.e20, HistoInfoC6D6);
-
-      FillType3(thisPulseType, NC6D6Signals, theC6D6Signals, h2D_C6D6, -1.e20,
-                1.e20, HistoInfoC6D6);
-
-      FillType4(thisPulseType, NC6D6Signals, theC6D6Signals, h2D_C6D6, -1.e20,
-                1.e20, HistoInfoC6D6);
-      FillType5(thisPulseType, NC6D6Signals, theC6D6Signals, h2D_C6D6, -1.e20,
-                1.e20, HistoInfoC6D6);
-      FillType6(thisPulseType, NC6D6Signals, theC6D6Signals, h2D_C6D6, -1.e20,
-                1.e20, HistoInfoC6D6);
-    }
     if (ThereIsSILIDetector) {
       NSILISignals =
           GetSignals(HistoInfoSILI->DetectorName,
@@ -408,16 +267,6 @@ int main(int argc, char** argv) {
 
       FillTypeSILICounts(thisPulseType, NSILISignals, theSILISignals,
                          hPulIntens, -1.e20, 1.e20, HistoInfoSILI);
-    }
-    if (ThereIsFIMGDetector) {
-      NFIMGSignals =
-          GetSignals(HistoInfoFIMG->DetectorName,
-                     HistoInfoFIMG->DetectorDeadTime_ns, trFIMG, &theFIMGS,
-                     bunchesIndexFIMG[npul], bunchesIndexFIMG[npul + 1] - 1,
-                     theFIMGSignals, theCalibM, 1, FIMGTFlash);  //
-
-      FillType1(thisPulseType, NFIMGSignals, theFIMGSignals, h2D_FIMG, -1.e20,
-                1.e20, HistoInfoFIMG);
     }
 
     if (ThereIsLABRDetector) {
