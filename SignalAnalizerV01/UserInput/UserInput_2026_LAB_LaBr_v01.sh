aa4b11c7e7e304da2b7f4434b8170e0ab825c167
Valcayne <valcayne@gmail.com>
2026-05-19 16:37:17 +0200

    dr

---
+++ SignalAnalizerV01/UserInput/UserInput_2026_LAB_LaBr_v01.sh
@@ -0,0 +1,28 @@
+*******************************************************
+*                                                     *
+*   WARNING: Do not use '~' sign to start the line!   *
+*   It marks the beginning of the input parameters!   *
+*                                                     *
+*******************************************************
+
+
+                                               DETECTOR SPECIFIC PARAMETERS (Lines may be commented with '#' sign!)
+=====================================================================================================================================================
+==============================================================================
+DETECTOR   DETECTOR   DETECTOR STEP   TIMING    MIXED     EXPAND   SMOOTHING   TIME    G-FLASH    G-FLASH     G-FLASH    G-FLASH   BASELINE   BASELINE   AMPLITUDE   AMPLITUDE   AREA/AMP.   AREA/AMP.   SIGNAL WIDTH   SIGNAL WIDTH    NUMBER OF     PULSE SHAPE
+  NAME      NUMBER     CLASS   SIZE   FILTER   POLARITY   PULSES    FILTER     LIMIT   OPTION    THRESHOLD   MIN_WIDTH   WINDOW     OPTION     FILTER
+     OPTION     THRESHOLD   LOW THR.    HIGH THR.     LOW THR.       HIGH THR.    PULSE SHAPES     ADDRESS
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+LABR          1         PSA     70/1.5        0        0          0         0        2e8       0         1000.        100          0          1         600         0          5        0        100        10/30           2000              0
+LABR          2         PSA     70/1.5        0        0          0         0        2e8       0         1000.        100          0          1         600         0          5        0        100        10/30           2000              0
+LABR          3         PSA     70/1.5        0        0          0         0        2e8       0         1000.        100          0          1         600         0          5        0        100        10/30           2000              0
+LABR          4         PSA     70/1.5        0        0          0         0        2e8       0         1000.        100          0          1         600         0          5        0        100        10/30           2000              0
+LABR          5         PSA     70/1.5        0        0          0         0        2e8       0         1000.        100          0          1         600         0          5        0        100        10/30           2000              0
+LABR          6         PSA     70/1.5        0        0          0         0        2e8       0         1000.        100          0          1         600         0          5        0        100        10/30           2000              0
+LABR          7         PSA     70/1.5        0        0          0         0        2e8       0         1000.        100          0          1         600         0          5        0        100        10/30           2000              0
+  
+################### 
+
