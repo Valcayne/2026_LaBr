a58f23823e4e2535fe15843f6b417d894d6fbb8d
Valcayne <valcayne@gmail.com>
2026-05-20 17:41:54 +0200

    d

--- Make2DHistos/Make2DHistos.cc
+++ Make2DHistos/Make2DHistos.cc
@@ -87,8 +87,8 @@ int main(int argc, char** argv) {
   HistoInfo* HistoInfoLABR = new HistoInfo();
 
   HistoInfoLABR->DetectorName = "LABR";
-  HistoInfoLABR->DetectorNumber = {0, 1, 2, 3,  4,  5,  6,
-                                   7, 8, 9, 10, 11, 12, 13};
+  HistoInfoLABR->DetectorNumber = {0,  1,  2,  3,  4,  5,  6,  7,  8, 9,
+                                   10, 11, 12, 13, 14, 15, 16, 17, 18};
   HistoInfoLABR->DetectorDeadTime_ns = {
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   };
