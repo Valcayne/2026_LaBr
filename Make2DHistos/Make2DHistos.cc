5a19dfe5cca747793947301960d551b91252c7cf
Valcayne <valcayne@gmail.com>
2026-08-06 15:45:54 +0200

    d

--- Make2DHistos/Make2DHistos.cc
+++ Make2DHistos/Make2DHistos.cc
@@ -85,8 +85,8 @@ int main(int argc, char** argv) {
   // Define FC-U
   HistoInfos->emplace_back();
   HistoInfos->back().DetectorName = "FC-U";
-  HistoInfos->back().DetectorNumber = {0, 1, 2, 3, 4, 5, 6, 7};
-  HistoInfos->back().DetectorDeadTime_ns = {0, 0, 0, 0, 0, 0, 0, 0};
+  HistoInfos->back().DetectorNumber = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+  HistoInfos->back().DetectorDeadTime_ns = {0, 0, 0, 0, 0, 0, 0, 0, 0};
   HistoInfos->back().UseAreaInsteadOfAmplitude = {false};  //
 
   HistoInfos->back().HistoName.push_back("Edep");
